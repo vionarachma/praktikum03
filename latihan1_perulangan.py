18b8281b215c109b16c94fc18a60d3aecaf4dae9
vionarachma <92927492+vionarachma@users.noreply.github.com>
2021-11-18 09:32:45 +0700

    Add files via upload

---
+++ latihan1_perulangan.py
@@ -0,0 +1,12 @@
+print("====================================")
+print("  Viona Rachma Haji Sahbrina (312110373)")
+print("    Program Bilangan Bertingkat")
+print("====================================")
+
+for i in range(10):
+    for j in range(10):
+       print('%2d'%(i + j), end=' ')
+    print()
+
+
+    
